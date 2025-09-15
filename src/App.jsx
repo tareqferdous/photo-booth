2ff58f102ffbcbfebe3c8a535825f2f1dcd6d1ee
tareqferdous <16101017@uap-bd.edu>
2025-09-15 11:23:46 +0600

    create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1 className="text-3xl font-bold underline">Hello world!</h1>,
+  },
+]);
+
 function App() {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
